1f838d24230b55dc5f8afa3fc15cdb2eeec4cd3f
Edurb23 <eddurb64@gmail.com>
2025-07-14 04:45:34 -0300

    Adicionar arquivos de projeto.

---
+++ Microondas-API/Models/ProgamaAquecimento.cs
@@ -0,0 +1,17 @@
+﻿namespace Microondas_API.Models
+{
+    public class ProgamaAquecimento
+    {
+        public class ProgramaAquecimento
+        {
+            public string Nome { get; set; }
+            public string Alimento { get; set; }
+            public int TempoEmSegundos { get; set; }
+            public int Potencia { get; set; }
+            public char Caractere { get; set; }
+            public string Instrucoes { get; set; }
+            public bool PreDefinido { get; set; } = false;
+        }
+
+    }
+}
